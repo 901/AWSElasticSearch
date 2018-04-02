8029e5c5fdea5d38720ee9ef57877c985f1ba697
Aditya Geria <adgeria@gmail.com>
2018-04-02 05:22:52 -0400

    added better search functionality + comments

--- src/main/java/com/amazonaws/lambda/gbif/GbifLambdaHandler.java
+++ src/main/java/com/amazonaws/lambda/gbif/GbifLambdaHandler.java
@@ -5,10 +5,8 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.URI;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
@@ -42,33 +40,51 @@ public class GbifLambdaHandler implements RequestHandler<Object, String> {
 	private static final String pretty = "true";
 	private static String responseValue; //contains string value of AmazonHttpResponse (contains results of query)
 	
-	/* Query String params possible:
+	/**
+	 * handleRequest is the AWS Lambda Handler for any API Requests coming in to .../gbif
+	 * Generates a requests based on the incoming query to search the Global Biodiversity Information Facility Database
+	 * Queries an ElasticSearch instance using an AWS4 Signed Query
+	 * @param input = API Request event input {LinkedHashMap}
+	 * @param context = context of the environment and execution
+	 * Query String parameters possible:
 	 * key sex sciName kingdom phylum class order family 
 	 * genus species country vernacularName year rightsHolder
 	 */
     public String handleRequest(Object input, Context context) {
+    	//converts the incoming input to a Json using Google's GSON library
     	JsonParser parser = new JsonParser();
     	String jsonString = new Gson().toJson(input, Map.class);
     	JsonObject obj = parser.parse(jsonString).getAsJsonObject();
-    	if(obj.get("params").getAsJsonObject().get("querystring").getAsString().length() < 3) {
+    	
+    	//test if map is empty, then return full database if so
+    	if(obj.get("params").getAsJsonObject().get("querystring").getAsJsonObject().size()==0) {
     		System.out.println("in map-empty");
     		Request<?> request = fullRequest();
     		performSigningSteps(request);
     		sendRequest(request);
     		return responseValue.toString();
     	}
-    	System.out.println("created map");
-    	System.out.println(generatePayload(obj));
-    	System.out.println(obj.get("params"));
-    	System.out.println(obj.get("params").getAsJsonObject().get("querystring"));
+    	
+    	//creates a request from the queries provided (ex: matching Kingdoms and Sex)
 		Request<?> request = generateRequest(obj.get("params").getAsJsonObject().get("querystring"));
 		System.out.println("made request");
+		
+		//signs the request using AWS4
 		performSigningSteps(request);
 		System.out.println("signed request");
+		
+		//make the request
 		sendRequest(request);
 		return responseValue.toString();
     }
 	
+    /**
+     * Sets the headers and parameters for the AWS Request
+     * content must contain the ElasticSearch query body (generated using generatePayload())
+     * Sets the endpoint, headers, and parameters for query (if any)
+     * @param map Json mapping of query
+     * @return AWS Request object
+     */
 	private static Request<?> generateRequest(JsonElement map) {
 		Request<?> request = new DefaultRequest<Void>(service_name);
 		request.setContent(new ByteArrayInputStream(generatePayload(map).getBytes()));
@@ -81,22 +97,39 @@ public class GbifLambdaHandler implements RequestHandler<Object, String> {
 		return request;
 	}
 	
+	/*
+	 * Similar to generateRequest, but makes a full request to the DB
+	 */
 	private static Request<?> fullRequest() {
 		Request<?> request = new DefaultRequest<Void>(service_name);
 		request.setContent(new ByteArrayInputStream("".getBytes()));
 		request.addHeader("Content-type", "application/json");
 		request.setEndpoint(URI.create(endpoint));
-		request.setResourcePath("/gbif/_search?q=*");
+		request.setResourcePath("/gbif/_search");
 		request.setHttpMethod(HttpMethodName.GET);
 		request.addParameter("pretty",pretty);
-		System.out.println(request.getResourcePath());
+		request.addParameter("q", "*");
 		return request;
 	}
 	
+	/**
+	 * Generates the ElasticSearch Query correlating to the HTTP Get query parameters
+	 * @param map query string mapping as key, value pairs
+	 * @return JSON formatted string in format of an ES query -> to be used as request content
+	 */
 	private static String generatePayload(JsonElement map) {
 		StringBuilder sb = new StringBuilder();
+		
 		for(Map.Entry<String, JsonElement> entry: map.getAsJsonObject().entrySet()) {
-			System.out.println(entry.getKey() + " " + entry.getValue());
+			/*
+			 * If a query includes a "sex" query, make sure to explicitly look for male vs female
+			 * because "male" can match with both "female" and "male" in ES.
+			 */
+			if(entry.getKey() == "sex") {		
+				String temp = "{\"term\" : { \"" + entry.getKey() + "\":\"" + entry.getValue() + "\"},";
+				sb.append(temp);
+				continue;
+			}
 			String temp = "{\"match\": {";
 			sb.append(temp);
 			temp = "\"" + entry.getKey() + "\":";
@@ -104,6 +137,7 @@ public class GbifLambdaHandler implements RequestHandler<Object, String> {
 			temp =  entry.getValue() + "}},";
 			sb.append(temp);
 		}
+		
 		sb.deleteCharAt(sb.toString().lastIndexOf(','));
 		String json = "{\"query\": {"+
 								"\"bool\": {"+ 
@@ -115,12 +149,18 @@ public class GbifLambdaHandler implements RequestHandler<Object, String> {
 		return json;
 	}
 	
+	/**
+	 * Performs AWS4 Signing steps according to AWS ElasticSearch/API Gateway Best Practices
+	 * Uses credentials stored in Environment Variables to authenticate as an authorized IAM user 
+	 * @param requestToSign request object to be signed
+	 * @return signed request (AWS Request Object)
+	 */
 	private static Request<?> performSigningSteps(Request<?> requestToSign) {
 		AWS4Signer signer = new AWS4Signer();
 		signer.setServiceName(service_name);
 		signer.setRegionName(regionName);
 		
-		//get credentials
+		//get credentials from environment
 		AWSCredentialsProvider credsProvider = new DefaultAWSCredentialsProviderChain();
 		AWSCredentials creds = credsProvider.getCredentials();
 		
@@ -128,6 +168,10 @@ public class GbifLambdaHandler implements RequestHandler<Object, String> {
 		return requestToSign;
 	}
 	
+	/**
+	 * Uses an AmazonHttpClient to execute a request (query) on the host (ES Cluster)
+	 * @param request
+	 */
 	private static void sendRequest(Request<?> request) {
 		ExecutionContext context = new ExecutionContext(true);
 		
@@ -139,6 +183,9 @@ public class GbifLambdaHandler implements RequestHandler<Object, String> {
 		Response<Void> response = client.execute(request, responseHandler, errorHandler, context);
 	}
 	
+	/*
+	 * Helper Method to convert an InputStream to a String
+	 */
 	public static String convertStreamToString(java.io.InputStream is) {
 		BufferedReader br = null;
 		StringBuilder sb = new StringBuilder();
